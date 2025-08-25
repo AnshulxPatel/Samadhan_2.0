cab2e9065e5c3fda18a6151e5f958cf85def6afa
Anshul Patel <0131CL231024@gmail.com>
2025-08-25 20:29:15 +0530

    Added code (student.js) and output

---
+++ student.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Anshul Patel",
+    age: 20,
+    course: "AIML",
+    college: "JNCT"
+};
+
+console.log("Student Details:");
+console.log(`Name: ${student.name}`);
+console.log(`Age: ${student.age}`);
+console.log(`Course: ${student.course}`);
+console.log(`College: ${student.college}`);
